cd6345818d65968aae64356955ee1d635c4f4274
Saranyaporn <sry.poilrn@gmail.com>
2026-09-09 13:39:00 +0700

    cm3 done hometask wait for addtask

--- app/hometask/page.tsx
+++ app/hometask/page.tsx
@@ -1,7 +1,29 @@
+import Image from "next/image";
+import AppName from "@/components/AppName";
+import Footer from "@/components/Footer";
+import Link from "next/link";
+
 export default function Page() {
   return (
-    <div>
-      <h1>HomeTask</h1>
+    <div className="w-full">
+      {/* แสดงชื่อแอป */}
+      <div className="text-center mt-20">
+        <AppName />
+      </div>
+      {/* แสดงรูป logo ของแอป */}
+      <Image
+        src="https://xeqtggywsdyrqsmxkatb.supabase.co/storage/v1/object/public/task_tb/task_logo.png"
+        alt="logo"
+        width={200}
+        height={200}
+        className="mx-auto mt-10"
+      />
+      {/* ปุ่มเปิดไปหน้า /addtask */}
+      <div className="w-4/5 text-end mt-10 mr-50">
+        <Link href="/addtask" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-6 rounded">
+          เพิ่ม TASK
+        </Link>
+      </div>
     </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,45 @@
+"use client";
 import AppName from "@/components/AppName";
 import Footer from "@/components/Footer";
 import Image from "next/image";
+import { useState } from "react";
+import Swal from "sweetalert2";
+import { useRouter } from "next/navigation";
 
 export default function Page() {
+  // สร้าง state สำหรับค่าจาก input ของ secure code
+  const [secureCode, setSecureCode] = useState("");
+
+  // สร้าง router สำหรับการนำทางไปยังหน้าอื่น
+  const router = useRouter();
+
+  // ฟังก์ชันสำหรับตรวจสอบ secure code และนำผู้ใช้ไปยังหน้า HomeTask
+  const handleAccessTask = () => {
+    if (secureCode === '') {
+      Swal.fire({
+        icon: 'warning',
+        title: 'คำเตือน',
+        text: 'กรุณากรอก secure code',
+      });
+
+      return;
+    }
+
+    // Check secure code
+    if (secureCode.toLocaleLowerCase() === 'dtisau') {
+      // เปิดไปหน้า /homeTask
+      // push() เปิดไปแล้วย้อนกลับได้ , replace() เปิดไปแล้วย้อนกลับไม่ได้
+      router.push('/hometask');
+    } else {
+      Swal.fire({
+        icon: 'warning',
+        title: 'ผิดพลาด',
+        // text: 'กรุณากรอกรหัสผ่านให้ถูกต้อง',
+        html: `<strong><u style="color:red">รหัสผ่านไม่ถูกต้อง</u></strong><br>กรุณากรอกรหัสผ่านให้ถูกต้อง`,
+      });
+    }
+  }
+  
   return (
     <div className="w-full">
       {/* แสดงชื่อแอป */}
@@ -22,9 +59,13 @@ export default function Page() {
         type="text"
         placeholder="Enter secure code"
         className="w-100 mx-auto mt-10 p-2 border rounded border-gray-400 flex"
+        value={secureCode}
+        onChange={(e) => setSecureCode(e.target.value)}
       />
       {/* ปุ่มเข้าใช้งานแอป */}
-      <button className="w-100 mx-auto mt-5 p-2 bg-blue-600 text-white rounded flex hover:bg-blue-700 mb-10">
+      <button className="w-100 mx-auto mt-5 p-2 bg-blue-600 text-white rounded flex hover:bg-blue-700 mb-10"
+        onClick={handleAccessTask}
+        >
         <span className="mx-auto">เข้าใช้งาน</span>
       </button>
       {/* Footer */}
