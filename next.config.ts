040111dd67acf6198ec48b9234287393e20b8ecc
Saranyaporn <sry.poilrn@gmail.com>
2026-09-09 11:23:03 +0700

    cm1 start project

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // ระบุตำแหน่งอ้างอิงรูปภาพจาก Network/Inter
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "xeqtggywsdyrqsmxkatb.supabase.co",
+        port: "",
+        pathname: '/**'
+      }
+    ]
+  }
 };
 
 export default nextConfig;
